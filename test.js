310def568a278a2ab311fddc8a3f0656d1a6dcb8
IgnacyWie <ignacywie@icloud.com>
2021-04-07 11:18:35 +0100

    Finished

---
+++ test.js
@@ -0,0 +1,39 @@
+const d = new Date();
+const hours = [
+    new Date(d.getFullYear(), d.getMonth(), d.getDate(), 08, 00),
+    new Date(d.getFullYear(), d.getMonth(), d.getDate(), 10, 35),
+    new Date(d.getFullYear(), d.getMonth(), d.getDate(), 12, 30),
+    new Date(d.getFullYear(), d.getMonth(), d.getDate(), 14, 35)
+]
+for(hour in hours){
+    var node = document.createElement("LI");                 // Create a <li> node
+    var textnode = document.createTextNode(`${hours[hour].getHours()}:${hours[hour].getMinutes()}`);         // Create a text node
+    node.appendChild(textnode); 
+    document.getElementById("now").appendChild(node);
+}
+
+var toDate;
+
+for(hour in hours){
+    if(hours[hour] > d){
+	toDate = hours[hour].getTime()
+	break
+    }
+}
+
+var mainLoop = setInterval(function(){
+    const d = new Date().getTime();
+    var distance = toDate - d
+
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    console.log(`${hours}:${minutes}:${seconds} to start of next lesson`)
+    document.getElementById("counter").innerHTML = hours + "h "
++ minutes + "m " + seconds + "s ";
+
+
+    if(distance < 0){
+	clearInterval(mainLoop);
+    }
+}, 1000)
