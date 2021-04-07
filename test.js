f74bb1ff267b2aa0af54128daa0ce2a96d958c94
IgnacyWie <ignacywie@icloud.com>
2021-04-07 11:34:13 +0100

    Fixed javascript error

--- test.js
+++ test.js
@@ -5,8 +5,8 @@ const hours = [
     new Date(d.getFullYear(), d.getMonth(), d.getDate(), 10, 35),
     new Date(d.getFullYear(), d.getMonth(), d.getDate(), 12, 10),
     new Date(d.getFullYear(), d.getMonth(), d.getDate(), 12, 30),
-    new Date(d.getFullYear(), d.getMonth(), d.getDate(), 14, 35)
-    new Date(d.getFullYear(), d.getMonth(), d.getDate(), 16, 10),
+    new Date(d.getFullYear(), d.getMonth(), d.getDate(), 14, 35),
+    new Date(d.getFullYear(), d.getMonth(), d.getDate(), 16, 10)
 ]
 for(hour in hours){
     var node = document.createElement("LI");
@@ -27,7 +27,6 @@ for(hour in hours){
 var mainLoop = setInterval(function(){
     const d = new Date().getTime();
     var distance = toDate - d
-
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
